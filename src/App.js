11e2ee1c8a70d0228ae1ff8b002fc35b29e3fed3
Cláudio Mindouro <claudiopedrom@gmail.com>
2021-01-01 15:40:32 +0100

    feat(canvas): loader and load animation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React, { Suspense, useState } from 'react'
 //Styles
 import './assets/styles/App.scss'
 //Three
-import { Canvas } from 'react-three-fiber'
+import { Canvas, useThree } from 'react-three-fiber'
 import Lights from './components/Three/lights'
 import Floor from './components/Three/floor'
-import { softShadows } from '@react-three/drei'
+import { softShadows, Loader, OrbitControls } from '@react-three/drei'
+import { useSpring } from 'react-spring'
 //Model
 import Model from './components/Three/chest'
 
 softShadows()
+
+// on load zoom effect
+const ZoomWithOrbital = () => {
+  const { gl, camera } = useThree()
+  useSpring({
+    from: {
+      z: 30,
+    },
+    x: -5,
+    y: 4,
+    z: 4,
+    // On Frame
+    onFrame: ({ x, y, z }) => {
+      camera.position.x = x
+      camera.position.y = y
+      camera.position.z = z
+    },
+  })
+  return <OrbitControls enableZoom={false} enablePan={false} target={[0, 0, 0]} args={[camera, gl.domElement]} />
+}
 
 const App = () => {
   const [open, setOpen] = useState(false)
@@ -21,8 +42,11 @@ const App = () => {
         <Suspense fallback={null}>
           <Model open={open} setOpen={setOpen} />
           <Floor />
+          <ZoomWithOrbital />
         </Suspense>
       </Canvas>
+      {/* Loader */}
+      <Loader />
     </>
   )
 }
